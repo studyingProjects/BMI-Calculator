23f87ab54aa8d766992b446df4ecf0c69be866a7
studyingProjects <148430835+studyingProjects@users.noreply.github.com>
2023-11-08 20:54:29 +0200

    UIDliders adjusting

--- BMI-Calculator-LayoutPractice/Controllers/ViewController.swift
+++ BMI-Calculator-LayoutPractice/Controllers/ViewController.swift
@@ -9,12 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    @IBOutlet weak var heightSlider: UISlider!
+    @IBOutlet weak var weightSlider: UISlider!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        heightSlider.value = 1.50
+        weightSlider.value = 100
     }
 
-
+    @IBAction func heightSliderChanged(_ sender: UISlider) {        
+        heightLabel.text = String(format: "%.2fm", sender.value)
+    }
+    
+    @IBAction func weightSliderChanged(_ sender: UISlider) {
+        weightLabel.text = String(format: "%.0fKg", sender.value)
+    }
+    
 }
 
